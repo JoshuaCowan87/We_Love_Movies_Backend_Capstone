2618bd0acc71cca0480a0bb5194c725e2c45d225
joshua cowan <joshuacowan87@gmail.com>
2021-10-22 20:46:43 -0700

    update readme

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-Sample project creating server and database.
+# We Love Movies
+Created database and inplemented REST API's as a sample project for a movie and critic review website. Since the focus of this project was exclusively backend, there is no front end component. I succesfully completed PostgreSQL database set up, migrations, seeding and SQL queries. Each query is set to mimic a search button or field, that would occur on a movie, critic and theatre website.
 
-Skills used: PostgreSQL, knex, ElephantSQL, dBeaver and RESTful APIs.
+## Tech Stack
+Backend: PostgreSQL, knex, Node.js, Express, CORS
 
-Basic Functionality: Sample project where I created and connected server and database, and write a variety of CRUD endpoints using RESTful APIs.
+## Improvements
+First improvment for this project would be to create an front end, to show the results of each query. Some of the middleware in the controller files can be refactored. For the purpose of this project, I needed to write each middleware seperately.
--- src/reviews/reviews.service.js
+++ src/reviews/reviews.service.js
@@ -1,52 +1,40 @@
 const knex = require("../db/connection");
-const mapProperties = require("../utils/map-properties")
+const mapProperties = require("../utils/map-properties");
 
 const addCritic = mapProperties({
-    
-    preferred_name: "critic.preferred_name",
-    surname: "critic.surname",
-    organization_name: "critic.organization_name",
-})
+  preferred_name: "critic.preferred_name",
+  surname: "critic.surname",
+  organization_name: "critic.organization_name",
+});
 
-function read (review_id) {
-return knex("reviews")
-    .select("*")
-    .where({review_id})
-    .first()  
-    
+function read(review_id) {
+  return knex("reviews").select("*").where({ review_id }).first();
 }
 
-
-function destroy (reviewId) {
-return knex("reviews")
-    .where({"review_id": reviewId})
-    .del()
+function destroy(reviewId) {
+  return knex("reviews").where({ review_id: reviewId }).del();
 }
 
-
-function update (updatedReview, review_id) {
-    return knex("reviews")
+function update(updatedReview, review_id) {
+  return knex("reviews")
     .select("*")
-    .where({review_id})
+    .where({ review_id })
     .update(updatedReview)
-    .then(updatedRecords => updatedRecords[0])
-    
+    .then((updatedRecords) => updatedRecords[0]);
 }
 
-
-function readUpdateWithCritic (review_id){
-return knex("reviews as r")
+function readUpdateWithCritic(review_id) {
+  return knex("reviews as r")
     .join("critics as c", "r.critic_id", "c.critic_id")
     .select("*")
-    .where({review_id})
+    .where({ review_id })
     .first()
-    .then(addCritic)
+    .then(addCritic);
 }
-
 
 module.exports = {
-    destroy,
-    update,
-   read,
-   readUpdateWithCritic
-}
+  destroy,
+  update,
+  read,
+  readUpdateWithCritic,
+};
